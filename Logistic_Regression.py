35d3b7856e6556574a6b4e77b999ab7b4e498639
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-26 11:45:10 -0600

    Feature scaling

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -13,10 +13,13 @@ y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature scaling
-
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
 
 # Train the Logistic Regression model on the Training set
 
