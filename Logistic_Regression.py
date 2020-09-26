730c1836969e4465e5427cd5b2942bd03aaee458
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-26 17:45:48 -0600

    Instantiate Confusion Matrix & accuracy objects

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -46,9 +46,11 @@ print(prediction)
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
 
-
 # Make the Confusion Matrix
-
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
 
 # Visualize the Training set results
 
