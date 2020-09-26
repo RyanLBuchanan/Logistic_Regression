5f78c60ffae12973b5ec0937964674d7efe78240
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-26 15:40:49 -0600

    Matrix of features should be columns 2 & 3 and therefore [:, 2:4]

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -8,12 +8,16 @@ import pandas as pd
 
 # Import dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 2:4].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+print(X_train)
+
+print(y_train)
 
 # Feature scaling
 from sklearn.preprocessing import StandardScaler
